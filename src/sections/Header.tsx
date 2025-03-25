526bbd2695235b2e20d7dbad5638f1e5de5fd3a0
Dinesh Sake <dineshsake12345@gmail.com>
2025-03-25 12:29:32 +0530

    Made header sticky

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center absolute top-3 w-full z-10">
+    <div className="flex justify-center items-center sticky top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">Home</a>
-        <a href="#" className="nav-item selected-tab">Projects</a>
+        <a href="#" className="nav-item">Projects</a>
         <a href="#" className="nav-item">About</a>
-        <a href="#" className="nav-item">Contact</a>
+        <a href="#" className="nav-item selected-tab">Contact</a>
       </nav>
     </div>
   )
