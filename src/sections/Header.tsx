8ce1025ed8244ea4d3d8b33182dd14227d81188c
Dinesh Sake <dineshsake12345@gmail.com>
2025-03-29 09:44:10 +0530

    Changed z-index of header section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center sticky top-3 w-full z-10">
+    <div className="sticky top-3 w-full z-30 flex justify-center items-center">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">Home</a>
         <a href="#" className="nav-item">Projects</a>
