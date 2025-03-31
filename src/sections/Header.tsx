ca6433a55a785132a0874d5fb43305eba08f5c32
Dinesh Sake <dineshsake12345@gmail.com>
2025-03-31 13:24:52 +0530

    Created footer section and resolved overflow problem

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,12 @@
 export const Header = () => {
   return (
-    <div className="sticky top-3 w-full z-30 flex justify-center items-center">
+    <header className="sticky top-3 w-full z-30 flex justify-center items-center">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">Home</a>
         <a href="#" className="nav-item">Projects</a>
         <a href="#" className="nav-item">About</a>
         <a href="#" className="nav-item selected-tab">Contact</a>
       </nav>
-    </div>
+    </header>
   )
 };
